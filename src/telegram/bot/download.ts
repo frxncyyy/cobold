d73ce9385358f5e54d44983f2293cf317a6e2f7d
Francesco <99586620+frxncyyy@users.noreply.github.com>
2025-07-13 13:02:50 +0200

    remove "user" chat filter

--- src/telegram/bot/download.ts
+++ src/telegram/bot/download.ts
@@ -19,7 +19,7 @@ import { evaluatorsFor } from "@/telegram/helpers/text"
 
 export const downloadDp = Dispatcher.child()
 
-downloadDp.onNewMessage(filters.chat("user"), async (msg) => {
+downloadDp.onNewMessage(async (msg) => {
     const { e, t } = await evaluatorsFor(msg.sender)
 
     if (msg.text === "meow") {
